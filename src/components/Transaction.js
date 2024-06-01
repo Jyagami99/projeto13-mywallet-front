3181c85584cade29ac7e78644ab1f7841d7b1fd1
João Marcelo Simão de Castro <jm.castro@usp.br>
2024-06-01 14:53:55 -0300

    feat: add more information in Transaction component

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,13 +1,49 @@
+import styled from "styled-components";
+
 function Transaction({ type, date, description, value }) {
   return (
-    <div>
-      <div>
-        <p>{date}</p>
-        <p>{description}</p>
-      </div>
-      <p>{value}</p>
-    </div>
+    <>
+      <Transition>
+        <div>
+          <Spam> {date + "  "}</Spam>
+          <div>
+            <p>{description} </p>
+          </div>
+        </div>
+        <Type color={type === "deposit" ? "green" : "red"}>
+          {value.toFixed(2).replace(".", ",")}
+        </Type>
+      </Transition>
+    </>
   );
 }
+
+const Transition = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  font-size: 16px;
+  div {
+    display: flex;
+    word-wrap: break-word;
+    overflow: hidden;
+    p {
+      text-overflow: ellipsis;
+      width: 100%;
+      white-space: nowrap;
+      overflow: hidden;
+    }
+  }
+`;
+
+const Spam = styled.div`
+  display: inline;
+  color: var(--date_color);
+  width: 60px;
+`;
+
+const Type = styled.li`
+  color: ${(props) => props.color};
+`;
 
 export default Transaction;
